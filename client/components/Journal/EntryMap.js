8a3e1243e102972812a279fc1c8dba5a6c1bf493
Amanda Henneberry <amandahenneberry@icloud.com>
2023-05-01 10:03:20 -0400

    date menu sorted with toggle

--- client/components/Journal/EntryMap.js
+++ client/components/Journal/EntryMap.js
@@ -1,5 +1,6 @@
 // import React, {useState} from 'react'
 import React from "react";
+import {useState} from 'react';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -10,34 +11,69 @@ import Row from 'react-bootstrap/Row';
 
 
 export const EntryMap = ({ entries, onEntryClick }) => {
+  const [sortBy, setSortBy] = useState(false)
 
     const handleClick = ({ target }) => {
         const entry = target.value;
         onEntryClick(entry);
     };
+
+    const handleSort = () =>{
+      sortBy === false ? setSortBy(true) : setSortBy(false)
+    } 
     
     
 
     return (
         <DropdownButton className='column'  size='lg' title='View Entry Log' variant="link">
-        {
-          entries
-          .sort((a,b) => {
-            return new Date(a.date) - new Date(b.date)
-          })
-          .map(entry => (
-            <div key={entry.id} >
-              <Row>
-                    <small>{entry.date}</small>
-                    <Button type ='button' size='sm' variant="link" value = {entry.id} onClick={handleClick}>               
-                    <div className="vr"></div>
-                    {entry.title.slice(0, 12)}...
-                  </Button>
-                </Row>
-                <Dropdown.Divider />
+          {sortBy === false ? (
+            <div>
+            <Button size="sm" variant="outline-secondary" onClick={handleSort}> sort by newest</Button>
+             {
+              entries
+              .sort((a,b) => {
+                return new Date(a.date) - new Date(b.date)
+              })
+              .map(entry => (
+                <div key={entry.id} >
+                  <Row>
+                        <small>{entry.date}</small>
+                        <Button type ='button' size='sm' variant="link" value = {entry.id} onClick={handleClick}>               
+                        <div className="vr"></div>
+                        {entry.title.slice(0, 12)}...
+                      </Button>
+                    </Row>
+                    <Dropdown.Divider />
+                </div>
+              ))
+            }
             </div>
-          ))
-        }
+          ) : (
+            <div>
+            <Button size="sm" variant="outline-secondary" onClick={handleSort}> sort by oldest</Button>
+            {
+              entries
+              .sort((a,b) => {
+                return new Date(a.date) - new Date(b.date)
+              })
+              .reverse()
+              .map(entry => (
+                <div key={entry.id} >
+                  <Row>
+                        <small>{entry.date}</small>
+                        <Button type ='button' size='sm' variant="link" value = {entry.id} onClick={handleClick}>               
+                        <div className="vr"></div>
+                        {entry.title.slice(0, 12)}...
+                      </Button>
+                    </Row>
+                    <Dropdown.Divider />
+                </div>
+              ))
+            }
+            </div>
+
+          )}
+       
       </DropdownButton >
       
     )
