e5e3fea45406c87fcc02794fa682443190d0acb7
Amanda Henneberry <amandahenneberry@icloud.com>
2023-05-01 08:51:46 -0400

    entry menu styling

--- client/components/Journal/EntryMap.js
+++ client/components/Journal/EntryMap.js
@@ -4,6 +4,7 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
 
 
 
@@ -13,21 +14,22 @@ export const EntryMap = ({ entries, onEntryClick }) => {
     const handleClick = ({ target }) => {
         const entry = target.value;
         onEntryClick(entry);
-      };
-
+    };
+    
     
 
-      
     return (
         <DropdownButton className='column'  size='lg' title='View Entry Log' variant="link">
         {
           entries.map(entry => (
             <div key={entry.id} >
-                <Button type ='button' size='sm' variant="link" value = {entry.id} onClick={handleClick}>               
-                    {entry.date}
+              <Row>
+                    <small>{entry.date}</small>
+                    <Button type ='button' size='sm' variant="link" value = {entry.id} onClick={handleClick}>               
                     <div className="vr"></div>
                     {entry.title.slice(0, 12)}...
                   </Button>
+                </Row>
                 <Dropdown.Divider />
             </div>
           ))
