c203a2e26957ef28819973e96543eb971b2ae396
Amanda Henneberry <amandahenneberry@icloud.com>
2023-05-24 14:09:32 -0400

    some clean-up

--- client/components/ToDoList/ToDos.js
+++ client/components/ToDoList/ToDos.js
@@ -7,49 +7,57 @@ import { useDispatch } from "react-redux";
 import { postTodoThunk, deleteTodo, editTodo, fetchTodo } from "../../store";
 
 export const ToDos = ({ todos, userId }) =>{
-  const [newTask, setNewTask] = useState({});
-  const [edit, setEdit] = useState(false);
-  const [selectTask, setSelectTask] = useState('')
-  // const [todo, setTodo] = useState([])
+ 
   const dispatch = useDispatch();
    
- 
-  const handleChange = ({ target }) => {
-      const { name, value } = target;
-      setNewTask((prev) =>({ 
-        ...prev, 
-        [name]: value,
-        userId: userId
-      }));
+ //ADD TASKS
+ const [newTask, setNewTask] = useState({});
+
+ const handleChange = ({ target }) => {
+  const { name, value } = target;
+    setNewTask((prev) =>({ 
+      ...prev, 
+      [name]: value,
+      userId: userId
+    }));
   };
 
   const handleSubmit= (event) => {
-  event.preventDefault();
-  if (!newTask.taskName) return;
-  dispatch(postTodoThunk(newTask));
-  setNewTask({});
+event.preventDefault();
+if (!newTask.taskName) return;
+dispatch(postTodoThunk(newTask));
+setNewTask({});
   };
+
+
+
+
+//  EDIT A TASK
+  const [edit, setEdit] = useState(false);
+  const [selectTask, setSelectTask] = useState('')
+  const [todo, setTodo] = useState({})
 
   const handleSelect = (task) =>{
     setSelectTask(task);
     dispatch(fetchTodo(task.id));
     setEdit(true);
-  }
+    }
 
   const handleEditChange=(evt)=>{
     evt.preventDefault();
     setSelectTask({
         [evt.target.name]: evt.target.value
     })
-}
+  }
 
-  useEffect(()=> console.log('task selected: '+ selectTask.taskName), [selectTask]
-  )
+  useEffect(()=> console.log('task selected: '+ selectTask.taskName), [selectTask])
 
+  
+  //DELETE A TASK
   const handleDeleteTask = (taskIdToRemove, event) => {
-  event.preventDefault();
-  dispatch(deleteTodo(taskIdToRemove));
-};
+    event.preventDefault();
+    dispatch(deleteTodo(taskIdToRemove));
+  };
 
   return(
     <Form onSubmit={handleSubmit}>
@@ -78,7 +86,7 @@ export const ToDos = ({ todos, userId }) =>{
 
 const mapStateToProps = (state) =>{
   return{
-      todo: state.auth.todo,
+      todo: state.auth.todo
   }
 }
 
