4aeadb45e86b656dda9ee2b27d200f608dd1eaf7
Amanda Henneberry <amandahenneberry@icloud.com>
2023-06-15 10:09:50 -0400

    styling footer

--- client/components/About.js
+++ client/components/About.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Accordion from 'react-bootstrap/Accordion'
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import Card from 'react-bootstrap/Card';
 
-function CustomToggle({ children,  eventKey}) {
+function CustomToggle({ children,  eventKey, toggle, setToggle}) {
+    
+
     const decoratedOnClick = useAccordionButton(eventKey, () =>
       console.log('displaying about | contact...'),
     );
+
+    const [hoverX, onHoverX] = useState(false)
+  
+
     return (
         <button
+        onMouseEnter={()=>{
+            onHoverX(true);
+          }}
+           onMouseLeave={()=>{
+            onHoverX(false);
+           }}
           type="button"
-          style={{ backgroundColor: 'pink' }}
-          onClick={decoratedOnClick}
+          style={{ backgroundColor: 'transparent', float: 'right', borderColor:'transparent', outline: 'none', color: hoverX ? 'black' : 'white' }}
+          onClick={()=>{decoratedOnClick(); setToggle(!toggle)}}
         >
           {children}
         </button>
@@ -20,16 +32,20 @@ function CustomToggle({ children,  eventKey}) {
     
 
 const About = () =>{
+    const [toggle, setToggle] = useState(false)
+
     return(
-        <Accordion > 
-            <Card>
+        <Accordion> 
+            <Card bsstyle='default' style={{ borderColor:'transparent', outline: 'none', backgroundColor: 'transparent'}}>
                 <Card.Header>
-                    <CustomToggle eventKey="0">about | contact &nbsp; &nbsp;</CustomToggle>
+                    <CustomToggle eventKey="0" toggle={toggle} setToggle={setToggle}>
+                        { !toggle ? (<>about | contact &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp;</>) : (<>down arrow &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;</>) }
+                        </CustomToggle>
                     
 
                 </Card.Header>
                 <Accordion.Collapse eventKey="0">
-                <Card.Body>
+                <Card.Body style={{backgroundColor: 'white', margin: '10px'}}>
                     ABOUT ME PAGE 
                         !!!!
                 </Card.Body>
