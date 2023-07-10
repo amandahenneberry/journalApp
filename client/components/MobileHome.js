eada9cdbf1073e4b38d7b587e359539a1a4dbaae
Amanda Henneberry <amandahenneberry@icloud.com>
2023-07-10 14:48:28 -0400

    some content back in mobileHome

--- client/components/MobileHome.js
+++ client/components/MobileHome.js
@@ -141,7 +141,49 @@ const [latitude, setLatitude] = useState(0);
 
 return (
   <Container fluid>
-  <h1>TEST mobile home</h1>
+    <Row>
+        <Col>
+            <div className= "dateContainer">
+                <DateTime date={date.toLocaleDateString()} time={date.toLocaleTimeString()}/>
+            </div>
+        </Col>
+        <Col xs={6} style={{display: 'flex', padding:'10% 50% 10% 50%'}}>  
+    <Row>
+      <Tabs
+      defaultActiveKey="journal"
+      transition={false}
+      id="noanim-tab-example"
+      className="tabBar"
+      activeKey={active}
+      onSelect={handleSelect}
+    >
+      <Tab eventKey="journal" title="Journal">
+      <center>
+        <div className='journalBg'> 
+          <div className='paperBg'>
+            <Journal username ={username} entries={entries} handleClick={handleClick} cityName={cityName} weatherIcon={icon} description={description} high={high} low={low}/>  
+          </div>
+        </div>
+      </center>   
+      </Tab> 
+      
+      <Tab eventKey="toDos" title="To-Do List">
+      <div className='ToDoBg'> 
+      <ToDos todos={todos} userId={id} setActive={setActive} />
+      </div>
+      </Tab>  
+      
+      </Tabs>
+      </Row>
+    </Col>
+
+    <Col>
+    <div className="weatherContainer">
+      <Weather loadingWeather={loadingWeather} cityName={cityName} temperature={temperature} description={description} high={high} low={low} icon={icon}/>
+    </div>
+    </Col>
+    </Row>
+  
   </Container>
 )
 
