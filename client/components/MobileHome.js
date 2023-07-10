5c494580257e6bb8917b108aea7b2111e2d2424a
Amanda Henneberry <amandahenneberry@icloud.com>
2023-07-10 15:45:54 -0400

    added fluid container around tabs

--- client/components/MobileHome.js
+++ client/components/MobileHome.js
@@ -148,9 +148,33 @@ return (
             </div>
         </Col>
         <Col xs={6} style={{display: 'flex'}}>
-           
-     
-       <center>JOURNAL COMPONENT HERE</center>
+            <Container fluid>
+        <Tabs
+      defaultActiveKey="journal"
+      transition={false}
+      id="noanim-tab-example"
+      className="tabBar"
+      activeKey={active}
+      onSelect={handleSelect}
+    >
+      <Tab eventKey="journal" title="Journal">
+      <center>
+        <div className='journalBg'> 
+          <div className='paperBg'>
+            <Journal username ={username} entries={entries} handleClick={handleClick} cityName={cityName} weatherIcon={icon} description={description} high={high} low={low}/>  
+          </div>
+        </div>
+      </center>   
+      </Tab> 
+      
+      <Tab eventKey="toDos" title="To-Do List">
+      <div className='ToDoBg'> 
+      <ToDos todos={todos} userId={id} setActive={setActive} />
+      </div>
+      </Tab>  
+      
+      </Tabs>
+      </Container>
      
      
         </Col>
